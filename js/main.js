8e289b89d14ca4401415ab154d4a7c253c5a348b
Alexey-pyh <alexey.pyh132@gmail.com>
2020-11-27 17:30:33 +0300

    +

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var button = document.querySelectorAll('.registration');
 var modal = document.querySelector('.modal');
 var closeBut = document.querySelector('.close');
-console.log(button);
 button[0].onclick = function() {
     event.preventDefault();
     modal.classList.add('active');
@@ -9,9 +8,6 @@ button[0].onclick = function() {
 button[1].onclick = function() {
     event.preventDefault();
     modal.classList.add('active');
-};
-button[3].onclick = function() {
-    event.preventDefault();
 };
 closeBut.onclick = function() {
     event.preventDefault();
